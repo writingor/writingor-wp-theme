edff7610cfc48fe5322db3ec549c8437c018fd94
Igor Writingor <writingor.site@gmail.com>
2023-05-06 16:34:22 +0200

    Edit page template

--- page.php
+++ page.php
@@ -1,13 +1,7 @@
 <? get_header() ?>
 
 <main class="writingor--main" id="writingor--main">
-        <div class="writingor--container">
-                <br>
-                <p><b><a href="/">Вернуться на главную</a></b></p>
-                <h1><? the_title() ?></h1>
-                <? the_content() ?>
-                <br>
-        </div>
+        <? the_content() ?>
 </main>
 
 <? get_footer() ?>
