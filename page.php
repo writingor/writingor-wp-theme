ee4e2891a7308b6f3365be75a5caa8a3575ab9f6
Igor Writingor <writingor.site@gmail.com>
2023-05-07 23:45:05 +0200

    Add template without container

--- page.php
+++ page.php
@@ -1,7 +1,11 @@
 <? get_header() ?>
 
 <main class="writingor--main" id="writingor--main">
-        <? the_content() ?>
+	<div class="writingor--container">
+	<h1><? the_title() ?></h1>
+	
+	<? the_content() ?>
+	</div>
 </main>
 
 <? get_footer() ?>
