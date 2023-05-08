f6f78c260056826bedd3c20074662e79a975f6a0
Igor Writingor <writingor.site@gmail.com>
2023-05-08 21:58:41 +0200

    upd

--- includes/acf-options-page.php
+++ includes/acf-options-page.php
@@ -44,8 +44,8 @@ if (function_exists('acf_add_options_page')) {
 
     foreach ($languages as $lang) {
         acf_add_options_sub_page([
-            'page_title' => __("Options", 'writingor') . " $lang",
-            'menu_title' => __("Options", 'writingor') . " $lang",
+            'page_title' => __("Options", 'writingor') . ' ' . strtoupper($lang),
+            'menu_title' => __("Options", 'writingor') . ' ' . strtoupper($lang),
             'menu_slug'  => "acf-options-$lang",
             'post_id'    => "acf_options_$lang",
             'parent'     => $parent['menu_slug']
