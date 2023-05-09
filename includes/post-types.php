2f205e25f1478d25b3a65df94f8e4b8d66ee7826
Igor Writingor <writingor.site@gmail.com>
2023-05-09 12:14:31 +0200

    Add category to post types

--- includes/post-types.php
+++ includes/post-types.php
@@ -6,31 +6,37 @@ function writingor__add_post_types() {
             'slug' => 'reviews',
             'singular_name' => __('Review', 'writingor'),
             'plural_name' => __('Reviews', 'writingor'),
+            'taxonomies' => []
         ],
         [
             'slug' => 'benefits',
             'singular_name' => __('Benefit', 'writingor'),
             'plural_name' => __('Benefits', 'writingor'),
+            'taxonomies' => []
         ],
         [
             'slug' => 'services',
             'singular_name' => __('Service', 'writingor'),
             'plural_name' => __('Services', 'writingor'),
+            'taxonomies' => []
         ],
         [
             'slug' => 'portfolio',
             'singular_name' => __('Portfolio', 'writingor'),
             'plural_name' => __('Portfolio', 'writingor'),
+            'taxonomies' => ['category']
         ],
         [
             'slug' => 'notebook',
             'singular_name' => __('Notebook', 'writingor'),
             'plural_name' => __('Notebook', 'writingor'),
+            'taxonomies' => ['category']
         ],
         [
             'slug' => 'blog',
             'singular_name' => __('Blog', 'writingor'),
             'plural_name' => __('Blog', 'writingor'),
+            'taxonomies' => ['category']
         ]
     ];
     
@@ -46,7 +52,8 @@ function writingor__add_post_types() {
                 'has_archive' => true,
                 'rewrite' => ['slug' => $params['slug']],
                 'show_in_rest' => true,
-                'supports' => ['title', 'excerpt', 'editor', 'author', 'thumbnail', 'revisions', 'custom-fields']
+                'supports' => ['title', 'excerpt', 'editor', 'author', 'thumbnail', 'revisions', 'custom-fields'],
+                'taxonomies' => $params['taxonomies']
             ]
         );
     }
