12d4f03d68eb07e2bcaa7022d22f8e1cd50aac9c
Igor Writingor <writingor.site@gmail.com>
2023-05-09 15:34:30 +0200

    Add layout 10

--- woocommerce.php
+++ woocommerce.php
@@ -1,9 +1,16 @@
 <? get_header() ?>
 
 <main class="writingor--main" id="writingor--main">
-    <div class="writingor--layout-9">
-        <div class="writingor--layout-9__container writingor--container">
-            <? woocommerce_content() ?>
+    <div class="writingor--layout-10">
+        <div class="writingor--layout-10__container writingor--container">
+            <div class="writingor--layout-10__body">
+                <div class="writingor--layout-10__col-1">
+                    <? get_sidebar() ?>
+                </div>
+                <div class="writingor--layout-10__col-2">
+                    <? woocommerce_content() ?>
+                </div>
+            </div>
         </div>
     </div>
 </main>
