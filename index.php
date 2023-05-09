6dd8216c41b9daf930cd3e3ba29c11eecb7b0292
Igor Writingor <writingor.site@gmail.com>
2023-05-09 11:13:06 +0200

    Remove deprecated files and add layout-9

--- index.php
+++ index.php
@@ -5,15 +5,11 @@ $dir = get_template_directory_uri();
 <? get_header() ?>
 
 <main class="writingor--main" id="writingor--main">
-    <div class="writingor--container">
-        <br>
-        <br>
-        <br>
-        <p><b><a href="/"><?= esc_html__('To home page', 'writingor') ?></a></b></p>
-        <h1><? the_title() ?></h1>
-        <? the_content() ?>
-        <br>
-        <br>
+    <div class="writingor--layout-9">
+        <div class="writingor--layout-9__container writingor--container">
+            <h1><? the_title() ?></h1>
+            <? the_content() ?>
+        </div>
     </div>
 </main>
 
