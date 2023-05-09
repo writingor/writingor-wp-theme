926126c2e71d3c744e0bb5f0e5df864d137e6c16
Igor Writingor <writingor.site@gmail.com>
2023-05-09 17:56:22 +0200

    upd

--- includes/woocommerce-functions.php
+++ includes/woocommerce-functions.php
@@ -1,15 +1,4 @@
 <?
-
-/**
- * Add woocommerce
- */
-
-add_action('after_setup_theme', 'writingor__add_woocommerce_support');
-
-function writingor__add_woocommerce_support() {
-    add_theme_support('woocommerce');
-}
-
 /**
  * Fix login, register,
  * add to cart problem
