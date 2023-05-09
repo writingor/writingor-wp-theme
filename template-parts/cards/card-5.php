9f0d262c81bb26693ee78a33414a57b562e5eb35
Igor Writingor <writingor.site@gmail.com>
2023-05-09 11:43:04 +0200

    Add card 5 style

--- template-parts/cards/card-5.php
+++ template-parts/cards/card-5.php
@@ -1,6 +1,6 @@
 <!-- card 5 -->
 <div class="writingor--card-5">
-    <h2><?= esc_html(get_the_title()) ?></h2>
+    <h2><a href="<?= esc_url(get_the_permalink()) ?>"><?= esc_html(get_the_title()) ?></a></h2>
     <p>
         <? esc_html(get_the_excerpt()) ?>
     </p>
