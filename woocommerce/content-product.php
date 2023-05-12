cdca9652c929365fa87e8f3bfb3cdb63a05d2674
Igor Writingor <writingor.site@gmail.com>
2023-05-12 22:59:47 +0200

    test add class to product

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -24,7 +24,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php wc_product_class( '', $product ); ?>>
+<div <?php wc_product_class('writingor--card-6', $product); ?>>
 	<?php
 	/**
 	 * Hook: woocommerce_before_shop_loop_item.
@@ -64,4 +64,4 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 */
 	do_action( 'woocommerce_after_shop_loop_item' );
 	?>
-</li>
+</div>
