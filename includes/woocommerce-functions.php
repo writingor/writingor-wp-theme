646aa01951e6bdabade73b0b0e569565dd370e7f
Igor Writingor <writingor.site@gmail.com>
2023-05-12 23:15:16 +0200

    test 2

--- includes/woocommerce-functions.php
+++ includes/woocommerce-functions.php
@@ -252,4 +252,4 @@ add_filter('woocommerce_template_loop_product_link_open', function() {
 	$link = apply_filters('woocommerce_loop_product_link', get_the_permalink(), $product);
 
 	echo '<a href="' . esc_url( $link ) . '" class="writingor--card-6__cover woocommerce-LoopProduct-link woocommerce-loop-product__link">';
-}, 10, 2);
+}, 10);
