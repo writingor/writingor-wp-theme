593af39a62d7279ba7cf239cf2c380c825657d73
Igor Writingor <writingor.site@gmail.com>
2023-05-13 00:34:01 +0200

    test account-page wrapping

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) || exit;
  */
 do_action( 'woocommerce_account_navigation' ); ?>
 
-<div class="woocommerce-MyAccount-content">
+<div class="writingor--layout-10__col woocommerce-MyAccount-content">
 	<?php
 		/**
 		 * My Account content.
