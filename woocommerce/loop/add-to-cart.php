92b55846d510788e4221d5763f2d9162569a3e32
Igor Writingor <writingor.site@gmail.com>
2023-05-12 22:39:13 +0200

    change btn class

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -27,7 +27,7 @@ echo apply_filters(
 		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
 		esc_url($product->add_to_cart_url()),
 		esc_attr(isset($args['quantity']) ? $args['quantity'] : 1 ),
-		esc_attr(isset($args['class']) ? $args['class'] : 'writingor--button-1' ),
+		esc_attr(isset($args['class']) ? $args['class'] : 'writingor--button-3'),
 		isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
 		esc_html($product->add_to_cart_text())
 	),
