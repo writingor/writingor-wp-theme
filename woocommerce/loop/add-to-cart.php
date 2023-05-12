f6d1a6ad61eb49e07ea85d5b14afebdc2c4c1f9b
Igor Writingor <writingor.site@gmail.com>
2023-05-12 22:45:30 +0200

    Add class

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -24,10 +24,10 @@ global $product;
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+		'<a href="%s" data-quantity="writingor--button-3 %s" class="%s" %s>%s</a>',
 		esc_url($product->add_to_cart_url()),
 		esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
-		esc_attr(isset($args['class']) ? $args['class'] : 'writingor--button-3'),
+		esc_attr(isset($args['class']) ? $args['class'] : 'button'),
 		isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
 		esc_html($product->add_to_cart_text())
 	),
