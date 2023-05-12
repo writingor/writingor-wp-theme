3eec0dfd12a5aeb70f9a087bda727c642361b801
Igor Writingor <writingor.site@gmail.com>
2023-05-12 23:39:38 +0200

    replace price

--- includes/woocommerce-functions.php
+++ includes/woocommerce-functions.php
@@ -264,6 +264,30 @@ add_action('woocommerce_before_shop_loop_item', function() {
  * to woocommerce_after_shop_loop_item_title
  */
 
-remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
+remove_action(
+	'woocommerce_after_shop_loop_item',
+	'woocommerce_template_loop_product_link_close',
+	5
+);
 
-add_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 15);
+add_action(
+	'woocommerce_after_shop_loop_item_title',
+	'woocommerce_template_loop_product_link_close',
+	15
+);
+
+/**
+ * Replace price from title
+ */
+
+remove_action(
+	'woocommerce_after_shop_loop_item_title',
+	'woocommerce_template_loop_price',
+	5
+);
+
+add_action(
+	'woocommerce_shop_loop_item_title',
+	'woocommerce_template_loop_price',
+	15
+);
