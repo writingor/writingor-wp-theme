b4d1191f0cfb4ddab46296af5eef07f512f7c7d0
Igor Writingor <writingor.site@gmail.com>
2023-05-12 23:35:02 +0200

    replace tag

--- includes/woocommerce-functions.php
+++ includes/woocommerce-functions.php
@@ -258,12 +258,12 @@ add_action('woocommerce_before_shop_loop_item', function() {
 }, 10);
 
 /**
- * Remove closing a tag from
+ * Replace closing a tag from
  * woocommerce_after_shop_loop_item
+ * 
+ * to woocommerce_after_shop_loop_item_title
  */
 
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
 
-add_action('woocommerce_after_shop_loop_item_title', function() {
-	echo '</a>';
-}, 15);
+add_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 15);
