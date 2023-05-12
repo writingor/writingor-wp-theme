628df58f00e802e67ccaeb66cb2101f8fb5330e5
Igor Writingor <writingor.site@gmail.com>
2023-05-12 22:38:27 +0200

    test

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -24,12 +24,12 @@ global $product;
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" data-quantity="%s" class="writingor--button-1 %s" %s>%s</a>',
-		esc_url( $product->add_to_cart_url() ),
-		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		esc_html( $product->add_to_cart_text() )
+		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+		esc_url($product->add_to_cart_url()),
+		esc_attr(isset($args['quantity']) ? $args['quantity'] : 1 ),
+		esc_attr(isset($args['class']) ? $args['class'] : 'writingor--button-1' ),
+		isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
+		esc_html($product->add_to_cart_text())
 	),
 	$product,
 	$args
