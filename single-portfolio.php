9511a56d0944f0423b9b60aec0d68e40ed7bec93
Igor Writingor <writingor.site@gmail.com>
2023-05-12 18:36:29 +0200

    Add test single portfolio

---
+++ single-portfolio.php
@@ -0,0 +1,12 @@
+<? get_header() ?>
+
+<main class="writingor--main" id="writingor--main">
+    <div class="writingor--layout-9">
+        <div class="writingor--layout-9__container writingor--container">
+            <h1><? the_title() ?> (Portfolio)</h1>
+            <? the_content() ?>
+        </div>
+    </div>
+</main>
+
+<? get_footer() ?>
