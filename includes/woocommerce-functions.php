7ea298443d697bfc62ff0e56c74e1a4d8b3c600a
Igor Writingor <writingor.site@gmail.com>
2023-05-13 21:40:03 +0200

    test filter

--- includes/woocommerce-functions.php
+++ includes/woocommerce-functions.php
@@ -385,13 +385,22 @@ add_action(
  * all notices
  * op login page
  */
-remove_action('woocommerce_before_customer_login_form', 'woocommerce_output_all_notices', 10);
-add_action(
-	'woocommerce_before_customer_login_form', 
+// remove_action('woocommerce_before_customer_login_form', 'woocommerce_output_all_notices', 10);
+// add_action(
+// 	'woocommerce_before_customer_login_form', 
+// 	function () {
+// 		echo '<div class="writingor--notices-1 woocommerce-notices-wrapper">';
+// 		wc_print_notices();
+// 		echo '</div>';
+// 	},
+// 	10
+// );
+
+add_filter(
+	'woocommerce_output_all_notices',
 	function () {
 		echo '<div class="writingor--notices-1 woocommerce-notices-wrapper">';
 		wc_print_notices();
 		echo '</div>';
-	},
-	10
+	}
 );
