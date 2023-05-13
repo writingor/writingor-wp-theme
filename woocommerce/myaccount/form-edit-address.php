9123669d56bcece76e2edf92e75b8016b7255143
Igor Writingor <writingor.site@gmail.com>
2023-05-13 22:24:35 +0200

    restore

--- woocommerce/myaccount/form-edit-address.php
+++ woocommerce/myaccount/form-edit-address.php
@@ -19,11 +19,7 @@ defined('ABSPATH') || exit;
 
 $page_title = ('billing' === $load_address) ? esc_html__('Billing address', 'woocommerce') : esc_html__('Shipping address', 'woocommerce');
 
-// do_action('woocommerce_before_edit_account_address_form');
-?>
-slider
-<?
-writingor__woocommerce_output_all_notices();
+do_action('woocommerce_before_edit_account_address_form');
 ?>
 asd
 
