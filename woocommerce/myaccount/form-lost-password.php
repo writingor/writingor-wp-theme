cd7585ab80ca03ec27e1149a966ad8977fd5ffcd
Igor Writingor <writingor.site@gmail.com>
2023-05-13 21:15:00 +0200

    upd lost pwd page

--- woocommerce/myaccount/form-lost-password.php
+++ woocommerce/myaccount/form-lost-password.php
@@ -22,27 +22,54 @@ defined( 'ABSPATH' ) || exit;
 		<? do_action('woocommerce_before_lost_password_form') ?>
 
 		<div class="writingor--layout-10__body">
+			<div class="writingor--layout-10__col-1">
 
-			<form method="post" class="writingor--layout-10__col-1 woocommerce-ResetPassword lost_reset_password">
+				<!-- form 1 -->
+				<form
+					name="writingor__woo_register"
+					method="post"
+					class="writingor--form-1 woocommerce-ResetPassword lost_reset_password"
+				>
+					<h2 class="writingor--form-1__title">
+						<? esc_html_e('Reset password', 'writingor') ?>
+					</h2>
 
-				<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+					<?= apply_filters(
+						'woocommerce_lost_password_message',
+						esc_html__('Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce')
+					) ?>
 
-				<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-					<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?></label>
-					<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
-				</p>
+					<input
+						type="text"
+						placeholder="<? esc_html_e('Username or email', 'woocommerce') ?>"
+						class="woocommerce-Input woocommerce-Input--text input-text"
+						name="user_login"
+						id="user_login"
+						autocomplete="username"
+					>
 
-				<div class="clear"></div>
+					<?php do_action( 'woocommerce_lostpassword_form' ); ?>
 
-				<?php do_action( 'woocommerce_lostpassword_form' ); ?>
+					<input type="hidden" name="wc_reset_password" value="true"/>
 
-				<p class="woocommerce-form-row form-row">
-					<input type="hidden" name="wc_reset_password" value="true" />
-					<button type="submit" class="woocommerce-Button button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" value="<?php esc_attr_e( 'Reset password', 'woocommerce' ); ?>"><?php esc_html_e( 'Reset password', 'woocommerce' ); ?></button>
-				</p>
 
-				<?php wp_nonce_field( 'lost_password', 'woocommerce-lost-password-nonce' ); ?>
-			</form>
+					<?php wp_nonce_field( 'lost_password', 'woocommerce-lost-password-nonce' ); ?>
+
+					<button
+						data-text-1="<?= esc_attr__('Send', 'writingor') ?>"
+						data-text-2="<?= esc_attr__('Sending...', 'writingor') ?>"
+						type="submit"
+						class="writingor--button-4 woocommerce-Button button<? echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : '') ?>"
+						value="<? esc_attr_e('Reset password', 'woocommerce') ?>"
+					>
+						<? esc_html_e('Reset password', 'woocommerce' ) ?>
+					</button>
+
+					<p class="writingor--form-1__response-message"></p>
+
+				</form>
+				<!--/ form 1 -->
+			</div>
 
 			<div class="writingor--layout-10__col-2">
 				<!-- pass -->
