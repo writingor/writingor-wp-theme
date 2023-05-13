937c49e42a43424eeddb527f8aa62aae4833f571
Igor Writingor <writingor.site@gmail.com>
2023-05-13 22:56:13 +0200

    test

--- woocommerce/myaccount/form-edit-address.php
+++ woocommerce/myaccount/form-edit-address.php
@@ -40,7 +40,10 @@ do_action('woocommerce_before_edit_account_address_form');
 
 		<?
 		foreach ($address as $key => $field) {
-			// echo "<p>$key, $field</p>";
+			echo "<p>$key, $field</p>";
+			foreach ($field as $k => $v) {
+				echo "<p>$k, $v</p>";
+			}
 			woocommerce_form_field($key, $field, wc_get_post_data_by_key($key, $field['value']));
 		}
 		?>
